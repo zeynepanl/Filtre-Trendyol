b57fb54c9662b33439f5c67e981acffb3991dc7a
zeynepanl <zeynepanl29@gmanil.com>
2024-12-01 22:23:41 +0300

    code format

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,47 @@
-import { MagnifyingGlassIcon, UserIcon, HeartIcon, ShoppingCartIcon ,Bars3Icon,StarIcon, BoltIcon} from '@heroicons/react/24/solid';
-
+import {
+  MagnifyingGlassIcon,
+  UserIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+  Bars3Icon,
+  StarIcon,
+  BoltIcon,
+} from "@heroicons/react/24/solid";
 
 export default function Header() {
-    return (
-      <header>
-        {/* Reklam Bandı */}
-        <div className="bg-customPurple py-2 hidden md:block">
-          <div className="container mx-auto flex flex-col md:flex-row justify-between items-center  md:space-y-0 md:space-x-4 px-28">
-            {/* Sol Bölüm */}
-            <span
-              className="relative text-white md:text-xl font-extrabold  ml-20"
-              style={{
-                textShadow: `
+  return (
+    <header>
+      {/* Reklam Bandı */}
+      <div className="bg-customPurple py-2 hidden md:block">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center  md:space-y-0 md:space-x-4 px-28">
+          {/* Sol Bölüm */}
+          <span
+            className="relative text-white md:text-xl font-extrabold  ml-20"
+            style={{
+              textShadow: `
                   -1px -1px 2px #ff5f5f,   /* Sol üst - Pembe */
                   1px 1px 2px #5fafff,     /* Sağ alt - Mavi */
                   -1px 1px 2px #ab47bc,    /* Sol alt - Mor */
                   1px -1px 2px #ff5f5f     /* Sağ üst - Pembe */
                 `,
-              }}
-            >
-              27 KASIM - 2 ARALIK
-            </span>
-  
-            {/* Merkezi EFSANE GÜNLER Bölümü */}
-            <div className="bg-gradient-to-r from-pink-500 via-blue-500 to-purple-500 text-white font-bold md:text-2xl py-2 px-6 rounded-full shadow-lg">
-              EFSANE GÜNLER
-            </div>
-  
-            {/* Sağ Bölüm */}
-            <span className="text-white  md:text-4xl font-bold pr-24 tracking-tight">
-              KAÇIRIRSAN ÜZÜLÜRSÜN!
-            </span>
-          </div>
-        </div>
+            }}
+          >
+            27 KASIM - 2 ARALIK
+          </span>
 
-        {/* top bar */}
+          {/* Merkezi EFSANE GÜNLER Bölümü */}
+          <div className="bg-gradient-to-r from-pink-500 via-blue-500 to-purple-500 text-white font-bold md:text-2xl py-2 px-6 rounded-full shadow-lg">
+            EFSANE GÜNLER
+          </div>
+
+          {/* Sağ Bölüm */}
+          <span className="text-white  md:text-4xl font-bold pr-24 tracking-tight">
+            KAÇIRIRSAN ÜZÜLÜRSÜN!
+          </span>
+        </div>
+      </div>
+
+      {/* top bar */}
       <div className=" py-1 hidden md:block">
         <div className="container mx-auto flex justify-end space-x-6 text-xs text-gray-400 pr-44">
           <a href="#" className="hover:text-black transition duration-200">
@@ -65,19 +72,18 @@ export default function Header() {
           {/* Arama Çubuğu */}
           <div className="relative w-full max-w-xl">
             <input
-                type="text"
-                placeholder="Aradığınız ürün, kategori veya markayı yazınız"
-                className="w-full py-2 pl-4 pr-12 bg-[#f3f3f3] shadow-sm text-sm font-sans focus:outline-none focus:ring-2 focus:ring-orange-500"
+              type="text"
+              placeholder="Aradığınız ürün, kategori veya markayı yazınız"
+              className="w-full py-2 pl-4 pr-12 bg-[#f3f3f3] shadow-sm text-sm font-sans focus:outline-none focus:ring-2 focus:ring-orange-500"
             />
             <button className="absolute inset-y-0 right-0 flex items-center pr-4">
-            <MagnifyingGlassIcon  className="h-6 w-6 text-orange-500" />
+              <MagnifyingGlassIcon className="h-6 w-6 text-orange-500" />
             </button>
-            </div>
+          </div>
 
+          {/* Kullanıcı İkonları */}
 
-            {/* Kullanıcı İkonları */}
-
-            <div className="flex items-center space-x-6 text-xs font-semibold text-gray-700">
+          <div className="flex items-center space-x-6 text-xs font-semibold text-gray-700">
             {/* Giriş Yap */}
             <a
               href="#"
@@ -109,55 +115,81 @@ export default function Header() {
       </div>
 
       {/* Tüm Kategoriler ve Menü */}
-<div className="bg-white py-2 border-b border-gray-300">
-  <div className="container mx-auto flex items-center justify-between px-28 pr-44">
-    {/* Tüm Kategoriler */}
-    <div className="flex items-center space-x-2 ml-12">
-      <Bars3Icon className="w-5 h-5 text-black" />
-      <span className="text-sm font-semibold text-black">TÜM KATEGORİLER</span>
-    </div>
+      <div className="bg-white py-2 border-b border-gray-300">
+        <div className="container mx-auto flex items-center justify-between px-28 pr-44">
+          {/* Tüm Kategoriler */}
+          <div className="flex items-center space-x-2 ml-12">
+            <Bars3Icon className="w-5 h-5 text-black" />
+            <span className="text-sm font-semibold text-black">
+              TÜM KATEGORİLER
+            </span>
+          </div>
 
-    {/* Menü Linkleri */}
-    <div className="flex items-center justify-end space-x-6 text-xs font-semibold text-gray-700 flex-grow">
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Kadın
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Erkek
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Anne & Çocuk
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Ev & Yaşam
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Süpermarket
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Kozmetik
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Ayakkabı & Çanta
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Elektronik
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Çok Satanlar
-      </a>
-      <a href="#" className="hover:text-orange-500 transition duration-200">
-        Flaş Ürünler
-      </a>
-    </div>
-  </div>
-</div>
-
-
-
-
-
-      </header>
-    );
-  }
-  
+          {/* Menü Linkleri */}
+          <div className="flex items-center justify-end space-x-6 text-xs font-semibold text-gray-700 flex-grow">
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Kadın
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Erkek
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Anne & Çocuk
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Ev & Yaşam
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Süpermarket
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Kozmetik
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Ayakkabı & Çanta
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Elektronik
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Çok Satanlar
+            </a>
+            <a
+              href="#"
+              className="hover:text-orange-500 transition duration-200"
+            >
+              Flaş Ürünler
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
